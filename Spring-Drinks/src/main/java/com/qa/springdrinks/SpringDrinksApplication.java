b93f20e85065923cfb330527ce4c87ccad3a8ca4
HarryBourton <h_bourton@sky.com>
2022-01-04 12:26:53 +0000

    Committing created spring project

---
+++ Spring-Drinks/src/main/java/com/qa/springdrinks/SpringDrinksApplication.java
@@ -0,0 +1,13 @@
+package com.qa.springdrinks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDrinksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDrinksApplication.class, args);
+	}
+
+}
