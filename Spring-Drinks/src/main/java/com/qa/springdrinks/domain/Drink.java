9440dc6806318eb1d91d6d7e761a047e8b28d83b
HarryBourton <h_bourton@sky.com>
2022-01-07 15:24:55 +0000

    Added sql files for testing, also fixed an issue with the 'fizzy' attribute

--- Spring-Drinks/src/main/java/com/qa/springdrinks/domain/Drink.java
+++ Spring-Drinks/src/main/java/com/qa/springdrinks/domain/Drink.java
@@ -27,7 +27,7 @@ public class Drink {
 	private int volume;
 	
 	@NotNull(message = "Is the drink fizzy?")
-	private boolean isFizzy;
+	private boolean fizzy;
 	
 	@NotNull(message = "Does the drink contain sugar?")
 	private boolean containsSugar;
@@ -37,24 +37,24 @@ public class Drink {
 	}
 
 	//For testing
-	public Drink(int id, String name, String manufacturer, int yearIntroduced, int volume, boolean isFizzy, boolean containsSugar) {
+	public Drink(int id, String name, String manufacturer, int yearIntroduced, int volume, boolean fizzy, boolean containsSugar) {
 		super();
 		this.id = id;
 		this.name = name;
 		this.manufacturer = manufacturer;
 		this.yearIntroduced = yearIntroduced;
 		this.volume = volume;
-		this.isFizzy = isFizzy;
+		this.fizzy = fizzy;
 		this.containsSugar = containsSugar;
 	}
 	//For creating
-	public Drink(String name, String manufacturer, int yearIntroduced, int volume, boolean isFizzy, boolean containsSugar) {
+	public Drink(String name, String manufacturer, int yearIntroduced, int volume, boolean fizzy, boolean containsSugar) {
 		super();
 		this.name = name;
 		this.manufacturer = manufacturer;
 		this.yearIntroduced = yearIntroduced;
 		this.volume = volume;
-		this.isFizzy = isFizzy;
+		this.fizzy = fizzy;
 		this.containsSugar = containsSugar;
 	}
 	
@@ -89,10 +89,10 @@ public class Drink {
 		this.volume = volume;
 	}
 	public boolean isFizzy() {
-		return isFizzy;
+		return fizzy;
 	}
-	public void setFizzy(boolean isFizzy) {
-		this.isFizzy = isFizzy;
+	public void setFizzy(boolean fizzy) {
+		this.fizzy = fizzy;
 	}
 	public boolean isContainsSugar() {
 		return containsSugar;
@@ -102,7 +102,7 @@ public class Drink {
 	}
 	@Override
 	public int hashCode() {
-		return Objects.hash(containsSugar, id, isFizzy, manufacturer, name, volume, yearIntroduced);
+		return Objects.hash(containsSugar, id, fizzy, manufacturer, name, volume, yearIntroduced);
 	}
 	@Override
 	public boolean equals(Object obj) {
@@ -113,7 +113,7 @@ public class Drink {
 		if (getClass() != obj.getClass())
 			return false;
 		Drink other = (Drink) obj;
-		return containsSugar == other.containsSugar && id == other.id && isFizzy == other.isFizzy
+		return containsSugar == other.containsSugar && id == other.id && fizzy == other.fizzy
 				&& Objects.equals(manufacturer, other.manufacturer) && Objects.equals(name, other.name)
 				&& volume == other.volume && yearIntroduced == other.yearIntroduced;
 	}
